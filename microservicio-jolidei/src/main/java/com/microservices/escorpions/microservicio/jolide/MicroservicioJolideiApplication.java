f5218be71d63af85c22139f72b3533579d1a5d5c
scorpions <uni.ramos.telecomun@gmail.com>
2020-04-24 15:12:39 -0500

    microservicio prueba

---
+++ microservicio-jolidei/src/main/java/com/microservices/escorpions/microservicio/jolide/MicroservicioJolideiApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.escorpions.microservicio.jolide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioJolideiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioJolideiApplication.class, args);
+	}
+
+}
---
+++ microservicio-jolidei/src/main/java/com/microservices/escorpions/microservicio/jolide/controllers/PruebaController.java
@@ -0,0 +1,6 @@
+package com.microservices.escorpions.microservicio.jolide.controllers;
+
+public class PruebaController {
+//prueba controller
+	public int n;
+}
