ff632a2f22eb0c4d20119758ec6c631a2198cd32
scorpions <uni.ramos.telecomun@gmail.com>
2020-04-24 12:46:43 -0500

    servidor eureka incluido

---
+++ microbio-eureka/src/test/java/com/microservices/escorpions/app/microbioeureka/MicrobioEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.escorpions.app.microbioeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobioEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
